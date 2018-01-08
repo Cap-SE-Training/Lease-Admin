cfeae4ce7c37a389c1395d9a7078734fa0d04e14
Jimmy Knoot <jimmy.knoot@capgemini.com>
2018-01-08 16:02:20 +0100

    updated lease contract, removed id and added class as arguments instead

--- src/src/Leasecontract.java
+++ src/src/Leasecontract.java
@@ -1,23 +1,13 @@
 import java.util.Date;
 
 public class Leasecontract {
-    private int id;
     private Vehicle vehicle;
     private Employer employer;
     private Date startDate;
     private Date endDate;
     private int duration; // in minutes
 
-    public int getId() {
-        return this.id;
-    }
-
-    public int getVehicle(Vehicle vehicle) {
-        return this.vehicle;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public Leasecontract() {
     }
 
     public Vehicle getVehicle() {
