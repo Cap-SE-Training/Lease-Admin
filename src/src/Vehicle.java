5b9419ef68f68252ef781b6153d3e8561f4bea31
SAHURK <s.vandenhurk@gmail.com>
2018-01-08 15:58:03 +0100

    Add getters & setters

--- src/src/Vehicle.java
+++ src/src/Vehicle.java
@@ -1,10 +1,25 @@
 public class Vehicle {
 
     private String license;
-    private int id;
     private int mileage;
 
     public Vehicle(){
 
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
     }
 }
