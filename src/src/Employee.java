8d64b34e0c2328795ae74a9b62bc903ba7d9ce99
Alexander van 't Hof <alexander.vthof@gmail.com>
2018-01-08 15:16:07 +0100

    Employee class

--- src/src/Employee.java
+++ src/src/Employee.java
@@ -2,10 +2,35 @@ public class Employee {
 
     private String name;
     private int id;
-    private int contract_id;
+    private int contractId;
 
     public Employee(){
 
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(int contractId) {
+        this.contractId = contractId;
+    }
+
+
 }
---
+++ src/src/Leasecompany.java
@@ -0,0 +1,2 @@
+public class Leasecompany {
+}
