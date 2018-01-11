cd46364c9a05455576f9f2c830b7a417dfe12af1
SAHURK <s.vandenhurk@gmail.com>
2018-01-11 14:35:05 +0100

    changed constructor added drive function

--- src/main/java/com/capsetrack/leaseadmin/models/vehicle/Truck.java
+++ src/main/java/com/capsetrack/leaseadmin/models/vehicle/Truck.java
@@ -1,5 +1,6 @@
 package com.capsetrack.leaseadmin.models.vehicle;
 
+import com.capsetrack.leaseadmin.models.vehicle.VehicleInfo.EngineInfo;
 import com.capsetrack.leaseadmin.models.vehicle.enums.Fuel;
 import com.capsetrack.leaseadmin.models.vehicle.enums.Transmission;
 import com.capsetrack.leaseadmin.models.vehicle.enums.Brand;
@@ -10,20 +11,26 @@ public class Truck extends Vehicle {
 
     private boolean doubleLoader;
 
-    public Truck(String license, int mileage, Fuel fuelType, Brand brand, int addition, double price, Transmission transmission, ArrayList<String> options, double catalogValue, boolean dubbleLoader) {
-        super(license, mileage, fuelType, brand, addition, price, transmission, options, catalogValue);
-        this.doubleLoader = dubbleLoader;
+    public Truck(String license, Brand brand, int addition, double price, double catalogValue, EngineInfo engineInfo, boolean doubleLoader) {
+        super(license, brand, addition, price, catalogValue, engineInfo);
+        this.doubleLoader = doubleLoader;
     }
 
-    public Truck(String id, String license, int mileage, boolean doubleLoader){
+    public Truck(String id, String license, int mileage, boolean doubleLoader) {
         super(id, license, mileage);
         this.doubleLoader = doubleLoader;
     }
+
     public boolean isDoubleLoader() {
         return doubleLoader;
     }
 
     public void setDoubleLoader(boolean doubleLoader) {
         this.doubleLoader = doubleLoader;
+    }
+
+    @Override
+    public void drive(Vehicle vehicle, int distance) {
+        vehicle.getEngineInfo().setMileage(vehicle.getEngineInfo().getMileage()+distance);
     }
 }
