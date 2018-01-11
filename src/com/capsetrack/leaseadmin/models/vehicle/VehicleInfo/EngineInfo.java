00897e2ef87e3895e26b45cd70bce7b8f59b8454
SAHURK <s.vandenhurk@gmail.com>
2018-01-11 13:36:52 +0100

    add EngineInfo class reduced generator size of Vehicle class add isFree changed equals method

---
+++ src/com/capsetrack/leaseadmin/models/vehicle/VehicleInfo/EngineInfo.java
@@ -0,0 +1,43 @@
+package com.capsetrack.leaseadmin.models.vehicle.VehicleInfo;
+
+import com.capsetrack.leaseadmin.models.vehicle.enums.Fuel;
+import com.capsetrack.leaseadmin.models.vehicle.enums.Transmission;
+
+public class EngineInfo {
+    private int mileage;
+    private Fuel fuelType;
+    private Transmission transmission;
+
+    public EngineInfo(int mileage, Fuel fuelType, Transmission transmission) {
+        this.mileage = mileage;
+        this.fuelType = fuelType;
+        this.transmission = transmission;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public Fuel getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(Fuel fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Transmission getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+}
+
+
+
