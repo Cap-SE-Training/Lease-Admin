9e0c6c6ed35a2c82b25d12be0632cb65da2a9b72
SAHURK <s.vandenhurk@gmail.com>
2018-01-11 14:34:28 +0100

    changed constructor added drive function

--- src/main/java/com/capsetrack/leaseadmin/models/vehicle/Car.java
+++ src/main/java/com/capsetrack/leaseadmin/models/vehicle/Car.java
@@ -1,22 +1,20 @@
 package com.capsetrack.leaseadmin.models.vehicle;
 
-import com.capsetrack.leaseadmin.models.vehicle.enums.Fuel;
-import com.capsetrack.leaseadmin.models.vehicle.enums.Transmission;
+import com.capsetrack.leaseadmin.models.vehicle.VehicleInfo.EngineInfo;
 import com.capsetrack.leaseadmin.models.vehicle.enums.Brand;
 
-import java.util.ArrayList;
-
 public class Car extends Vehicle {
-    public Car(String license, int mileage, Fuel fuelType, Brand brand, int addition, double price, Transmission transmission, ArrayList<String> options, double catalogValue) {
-        super(license, mileage, fuelType, brand, addition, price, transmission, options, catalogValue);
+
+    public Car(String license, Brand brand, int addition, double price, double catalogValue, EngineInfo engineInfo) {
+        super(license, brand, addition, price, catalogValue, engineInfo);
     }
 
-    public Car(String id, String license, int mileage){
+    public Car(String id, String license, int mileage) {
         super(id, license, mileage);
     }
 
     @Override
     public void drive(Vehicle vehicle, int distance) {
-        
+        vehicle.getEngineInfo().setMileage(vehicle.getEngineInfo().getMileage()+distance);
     }
 }
